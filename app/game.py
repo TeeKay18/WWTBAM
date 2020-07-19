6e9da9857c31992760379cfd7dcc5b9ba5df6e3d
TeeKay18 <tommkar321@wp.pl>
2020-07-20 00:01:33 +0200

    Game partially implemented

--- app/game.py
+++ app/game.py
@@ -1,40 +1,51 @@
 from typing import List
 from pathlib import Path
+import codecs
+from random import choice, randint
 
 
 class Game:
 
     def __init__(self, guaranteed_questions: List[int]):
+        self.guaranteed_questions = guaranteed_questions
         self.question_number = 1
-        self.fifty_fifty = True
-        self.switch = True
-        self.double_dip = True
+        self.game_on = False
+        self.question_shown = False
+        self.question_answered = False
+        self.current_question = []
+        self.fifty_fifty = 0
+        self.switch = 0
+        self.double_dip = 0
         self.guaranteed_sum = 0
 
-        self.questions_path = Path("app") / "config.txt"
+        self.questions_path = Path("app") / "questions" / "questions.txt"
         self.music_path = Path("app") / "music"
 
-        self.common_symbols = ["q" + str(i) for i in range(5, 16)]
-        self.common_symbols_two = ["q1-5", "q6-11", "q7-12", "q8-13", "q9-14"]
+        common_symbols = ["q" + str(i) for i in range(5, 16)]
+        common_symbols_two = ["q1-5", "q6-11", "q7-12", "q8-13", "q9-14"]
 
-        self.question_theme_symbols = ["q1-5"] + self.common_symbols[1:]
-        self.question_themes = [self.music_path / (symbol + "music.mp3")
-                                for symbol in self.question_theme_symbols]
+        question_theme_symbols = ["q1-5"] + common_symbols[1:]
+        question_themes = [self.music_path / (symbol + "music.mp3")
+                           for symbol in question_theme_symbols]
 
-        self.correct_ans_symbols = ["q1-4"] + self.common_symbols
-        self.correct_ans_themes = [self.music_path / (symbol + "correct.mp3")
-                                   for symbol in self.correct_ans_symbols]
+        final_ans_symbols = common_symbols_two[1:] + ["q10-15"]
+        final_ans_themes = [self.music_path / (symbol + "final.mp3")
+                            for symbol in final_ans_symbols]
 
-        self.wrong_ans_symbols = self.common_symbols_two + ["q15"]
-        self.wrong_ans_themes = [self.music_path / (symbol + "wrong.mp3")
-                                 for symbol in self.wrong_ans_symbols]
+        correct_ans_symbols = ["q1-4"] + common_symbols
+        correct_ans_themes = [self.music_path / (symbol + "correct.mp3")
+                              for symbol in correct_ans_symbols]
 
-        self.lets_play_symbols = self.common_symbols_two + ["q10-15"]
-        self.lets_play_symbols.remove("q6-11")
-        self.lets_play_symbols.insert(1, "q6")
-        self.lets_play_symbols.insert(2, "q11")
-        self.lets_play_themes = [self.music_path / (symbol + "letsplay.mp3")
-                                 for symbol in self.lets_play_symbols]
+        wrong_ans_symbols = common_symbols_two + ["q10"] + ["q15"]
+        wrong_ans_themes = [self.music_path / (symbol + "wrong.mp3")
+                            for symbol in wrong_ans_symbols]
+
+        lets_play_symbols = common_symbols_two + ["q10-15"]
+        lets_play_symbols.remove("q6-11")
+        lets_play_symbols.insert(1, "q6")
+        lets_play_symbols.append("q11")
+        lets_play_themes = [self.music_path / (symbol + "letsplay.mp3")
+                            for symbol in lets_play_symbols]
 
         self.lifeline_themes = [self.music_path / "50-50.mp3",
                                 self.music_path / "doubledip1ans.mp3",
@@ -42,3 +53,66 @@ class Game:
                                 self.music_path / "doubledip2ans.mp3",
                                 self.music_path / "doubledip2guess.mp3",
                                 self.music_path / "switchthequestion.mp3"]
+
+        fst_guaranteed = guaranteed_questions[0]
+        snd_guaranteed = guaranteed_questions[1]
+        victory = guaranteed_questions[2]
+
+        lp_indices = [0] + [-1] * (fst_guaranteed-1) +\
+                     [(i-fst_guaranteed+1) % 7 for i in range(fst_guaranteed, snd_guaranteed+1)] +\
+                     [(i-snd_guaranteed+1) % 7 for i in range(snd_guaranteed+1, victory)]
+        q_indices = [0] * (fst_guaranteed) + [(i-fst_guaranteed+1) % 11
+                                              for i in range(fst_guaranteed, victory)]
+        fa_indices = [-1] * (fst_guaranteed) + [i % 5 for i in range(fst_guaranteed, victory)]
+        ca_indices = [0] * (fst_guaranteed-1) + [(i-fst_guaranteed+1) % 12
+                                                 for i in range(fst_guaranteed, victory+1)]
+        wa_indices = [0] * (fst_guaranteed-1) + [i % 5
+                                                 for i in range(fst_guaranteed, snd_guaranteed)] +\
+                                                [5] +\
+                                                [(i+1) % 5
+                                                 for i in range(snd_guaranteed, victory-1)] + [6]
+
+        self.music_settings = [[lets_play_themes[lp_indices[i]]
+                                if lp_indices[i] >= 0 else None,
+                                question_themes[q_indices[i]],
+                                final_ans_themes[fa_indices[i]]
+                                if fa_indices[i] >= 0 else None,
+                                correct_ans_themes[ca_indices[i]],
+                                wrong_ans_themes[wa_indices[i]]
+                                ] for i in range(victory)
+                               ]
+
+        with codecs.open(self.questions_path, "r", "utf-8") as questions_file:
+            questions_data = questions_file.read().splitlines()
+            self._questions = [[int(questions_data[i]),
+                               questions_data[i+1],
+                               questions_data[i+2],
+                               questions_data[i+3],
+                               questions_data[i+4],
+                               questions_data[i+5]]
+                               for i in range(0, int(len(questions_data)), 6)]
+            self._questions = sorted(self._questions, key=lambda x: x[0])
+
+    def choose_random_question(self) -> list:
+        question = choice(self._questions)
+        self._questions.remove(question)
+        self.current_question = question
+        return question
+
+    def answer_correct(self, answer: str, question: list) -> bool:
+        return answer == question[2]
+
+    def lifeline_fifty_fifty(self, question: list) -> list:
+        self.fifty_fifty = self.question_number
+        fst_wrong_answer = question[randint(3, 5)]
+        question.remove(fst_wrong_answer)
+        snd_wrong_answer = question[randint(3, 4)]
+        question.remove(snd_wrong_answer)
+        return [fst_wrong_answer, snd_wrong_answer]
+
+    def lifeline_switch(self):
+        self.switch = self.question_number
+        return self.choose_random_question()
+
+    def get_music_for_question(self):
+        return self.music_settings[self.question_number-1]
--- app/main.py
+++ app/main.py
@@ -1,23 +1,30 @@
-import pygame
 import codecs
+import pygame
+import random
+import threading
 import tkinter as tk
-from tkinter.font import Font
-from app.colors import white, purple, green, orange, blue
-from app.game import Game
+
 from pathlib import Path
 from PIL import Image, ImageTk
+from time import sleep
+from tkinter.font import Font
+from typing import List
+
+from app.colors import white, purple, green, orange, blue, black
+from app.game import Game
+
 
 app_folder = Path("app")
 times_new_roman = "Times New Roman"
 
 
-def play(file_path: str) -> None:
-    pygame.mixer.music.load(file_path)
+def play(file_path: Path) -> None:
+    pygame.mixer.music.load(str(file_path))
     pygame.mixer.music.play()
 
 
 def play_intro() -> None:
-    play(str(app_folder / "music" / "intro.mp3"))
+    play(app_folder / "music" / "intro.mp3")
 
 
 def resize_image(image: ImageTk.PhotoImage, width: int, height: int) -> ImageTk.PhotoImage:
@@ -31,7 +38,7 @@ def button_with_image(root: tk.Tk, img: ImageTk.PhotoImage) -> None:
     return button
 
 
-def set_lifelines():
+def set_lifelines() -> None:
     graphics_folder = app_folder / "graphics"
     width, height = 46, 30
 
@@ -56,16 +63,24 @@ def set_lifelines():
     doubledip_button.grid(row=0, column=2)
 
 
-def tree_listbox(root: tk.Frame, wdth: int, num_questions: int) -> tk.Listbox:
+def tree_listbox(root: tk.Frame, wdth: int, num_questions: int, new_name: str) -> tk.Listbox:
     tree_font = Font(family=times_new_roman, size=18)
     return tk.Listbox(root, height=num_questions,
                       bg=purple, fg=white,
                       width=wdth, font=tree_font,
-                      bd=0, highlightthickness=0)
+                      bd=0, highlightthickness=0,
+                      name=new_name)
 
 
-def parse_tree():
-    tree_frame = tk.Frame(game_frame)
+def update_game_tree(no_question: int) -> None:
+    print("NO QUESTION:", no_question)
+    localization = ".game_frame.tree_frame.current_question_lst"
+    gui.nametowidget(localization).delete(15-no_question, 15-no_question)
+    gui.nametowidget(localization).insert(15-no_question, " •")
+
+
+def parse_tree() -> List[int]:
+    tree_frame = tk.Frame(game_frame, name="tree_frame")
 
     with codecs.open(str(app_folder / "config.txt"), "r", "utf-8") as file:
         data = file.read()
@@ -77,9 +92,9 @@ def parse_tree():
         guaranteed_questions = parsed_data[len(parsed_data)-1].split(sep=" ")
         guaranteed_questions = [int(num) for num in guaranteed_questions] + [num_questions]
 
-        prices_lst = tree_listbox(tree_frame, 9, num_questions)
-        no_question_lst = tree_listbox(tree_frame, 2, num_questions)
-        current_question_lst = tree_listbox(tree_frame, 1, num_questions)
+        prices_lst = tree_listbox(tree_frame, 9, num_questions, "prices_lst")
+        no_question_lst = tree_listbox(tree_frame, 2, num_questions, "no_question_lst")
+        current_question_lst = tree_listbox(tree_frame, 1, num_questions, "current_question_lst")
 
         for i in range(len(prices)):
             end = "end"
@@ -97,6 +112,8 @@ def parse_tree():
 
         prices_lst.configure(justify="right")
         no_question_lst.configure(justify="right")
+
+        # This makes sure that tree is read-only
         prices_lst.bindtags((prices_lst, tree_frame, "all"))
         current_question_lst.bindtags((current_question_lst, tree_frame, "all"))
         no_question_lst.bindtags((no_question_lst, tree_frame, "all"))
@@ -104,12 +121,12 @@ def parse_tree():
     return guaranteed_questions
 
 
-def set_game_button():
+def set_game_button() -> None:
     button_font = Font(family=times_new_roman, size=18)
     button_frame = tk.Frame(game_frame)
     game_button = tk.Button(button_frame, bg=orange, fg=white,
                             text="Nowa gra", font=button_font, padx=17,
-                            command=lets_play)
+                            command=start_thread_lets_play)
     button_frame.grid(row=2, column=0)
     game_button.grid(row=0, column=0)
 
@@ -117,32 +134,191 @@ def set_game_button():
 def set_question_box() -> None:
     question_font = Font(family=times_new_roman, size=16)
     question_text = tk.Text(question_frame, height=2, font=question_font,
-                            bg=purple, fg=white, width=56, wrap="word")
+                            bg=purple, fg=white, width=56, wrap="word",
+                            name="question_text", state="disabled")
     question_text.grid(row=0, column=0, padx=14, pady=12)
 
 
-def answer_text(root: tk.Frame, fnt: Font) -> tk.Text:
+def answer_text(root: tk.Frame, fnt: Font, new_name: str) -> tk.Text:
     return tk.Text(root, height=1, font=fnt,
-                   bg=purple, fg=white, width=34)
+                   bg=purple, fg=white, width=34,
+                   name=new_name, state="disabled")
 
 
-def set_answer_boxes():
+def confirm_final_answer(widget: tk.Text) -> None:
+    widget.configure(state="normal")
+    widget.configure(bg=orange)
+    widget.configure(state="disabled")
+
+
+def show_correct_answer(widget: tk.Text) -> None:
+    widget.configure(state="normal")
+    widget.configure(bg=green)
+    widget.configure(state="disabled")
+
+
+def clear_question_and_answer_boxes():
+    question_localization = ".question_frame.question_text"
+    answers_localization = ".question_frame.answers_frame.answer_"
+    answers = ["A", "B", "C", "D"]
+    gui.nametowidget(question_localization).config(state="normal")
+    gui.nametowidget(question_localization).delete("1.0", "end")
+    gui.nametowidget(question_localization).config(state="disabled")
+    for letter in answers:
+        gui.nametowidget(answers_localization + letter).config(state="normal", bg=purple)
+        gui.nametowidget(answers_localization + letter).delete("1.0", "end")
+        gui.nametowidget(answers_localization + letter).config(state="disabled")
+
+
+def green_answer(answers: List[str], localization: str):
+    for answer in answers:
+        ans_widget = gui.nametowidget(localization + answer)
+        ans_text = ans_widget.get("1.0", "end-1c")
+        if game.answer_correct(ans_text, game.current_question):
+            show_correct_answer(ans_widget)
+
+
+def verify_correct_answer(game: Game, user_answer: str,
+                          answers: List[str], localization: str) -> None:
+    green_answer(answers, localization)
+    if game.answer_correct(user_answer, game.current_question):
+        play(game.get_music_for_question()[3])
+        time_next_question = 0
+        if game.question_number < game.guaranteed_questions[0]:
+            time_next_question = 1500
+        elif game.question_number == game.guaranteed_questions[0]:
+            time_next_question = 10000
+        elif game.question_number < game.guaranteed_questions[1]:
+            time_next_question = 5000
+        elif game.question_number == game.guaranteed_questions[1]:
+            time_next_question = 10000
+        else:
+            time_next_question = 7500
+
+        game.question_answered = False
+        game.question_number += 1
+        gui.after(time_next_question, lambda: start_thread_lets_play())
+    else:
+        play(game.get_music_for_question()[4])
+        gui.after(3500, lambda: clear_question_and_answer_boxes())
+        game.game_on = False
+
+
+def check_answer_correct(button_name: str, game: Game) -> None:
+    localization = ".question_frame.answers_frame.answer_"
+    if game.game_on and game.question_shown and not game.question_answered:
+        game.question_answered = True
+        letter = button_name[len(button_name)-1]
+        answer_widget = gui.nametowidget(localization + letter)
+
+        confirm_final_answer(answer_widget)
+        final_ans_sound = game.get_music_for_question()[2]
+        if final_ans_sound is not None:
+            play(final_ans_sound)
+
+        user_answer = answer_widget.get("1.0", "end-1c")
+
+        answers = ["A", "B", "C", "D"]
+        time_verification = 0
+        if game.question_number < game.guaranteed_questions[0]:
+            time_verification = 500
+        elif game.question_number == game.guaranteed_questions[0]:
+            time_verification = 1500
+        elif game.question_number < game.guaranteed_questions[1]:
+            time_verification = 4500
+        elif game.question_number == game.guaranteed_questions[2]:
+            time_verification = 6000
+        else:
+            time_verification = 7000
+        gui.after(time_verification, lambda: verify_correct_answer(game, user_answer, answers, localization))
+
+
+def answer_button(root: tk.Frame, new_name: str, game: Game) -> tk.Button:
+    button_font = Font(family=times_new_roman, size=12)
+    letter = new_name[len(new_name)-1]
+    return tk.Button(root, name=new_name, bg=black, fg=white,
+                     font=button_font, width=3, height=1,
+                     text=letter, command=lambda: check_answer_correct(new_name, game))
+
+
+def set_answer_boxes(game: Game) -> None:
     answer_font = Font(family=times_new_roman, size=12)
-    answers_frame = tk.Frame(question_frame, bg=blue)
-    answer_A = answer_text(answers_frame, answer_font)
-    answer_B = answer_text(answers_frame, answer_font)
-    answer_C = answer_text(answers_frame, answer_font)
-    answer_D = answer_text(answers_frame, answer_font)   
-    answer_A.grid(row=0, column=0, padx=10, pady=12)
-    answer_B.grid(row=0, column=1)
-    answer_C.grid(row=1, column=0)
-    answer_D.grid(row=1, column=1)
+    answers_frame = tk.Frame(question_frame, bg=blue, name="answers_frame")
+    answer_A = answer_text(answers_frame, answer_font, "answer_A")
+    answer_B = answer_text(answers_frame, answer_font, "answer_B")
+    answer_C = answer_text(answers_frame, answer_font, "answer_C")
+    answer_D = answer_text(answers_frame, answer_font, "answer_D")
+
+    button_A = answer_button(answers_frame, "button_A", game)
+    button_B = answer_button(answers_frame, "button_B", game)
+    button_C = answer_button(answers_frame, "button_C", game)
+    button_D = answer_button(answers_frame, "button_D", game)
+
+    button_A.grid(row=0, column=0)
+    answer_A.grid(row=0, column=1)
+    button_B.grid(row=0, column=2)
+    answer_B.grid(row=0, column=3)
+    button_C.grid(row=1, column=0)
+    answer_C.grid(row=1, column=1)
+    button_D.grid(row=1, column=2)
+    answer_D.grid(row=1, column=3)
     answers_frame.grid(row=1, column=0)
 
 
+def start_thread_lets_play():
+    thread = threading.Thread(target=lets_play)
+    thread.daemon = True
+    thread.start()
+
+
+def insert_text(textbox: tk.Text, text: str) -> None:
+    textbox.configure(state="normal")
+    textbox.insert("end", text)
+    textbox.configure(state="disabled")
+
+
+def write_answer(question: list, indices: list, stop: int, end: int, answer_str: str) -> None:
+    localization = ".question_frame.answers_frame."
+    answer_index = random.choice(indices)
+    indices.remove(answer_index)
+    answer_widget = gui.nametowidget(localization + answer_str)
+    insert_text(answer_widget, question[answer_index])
+
+
+def write_question(question: list) -> None:
+    time_question = 5
+    time_answers = 1.5
+    question_widget = gui.nametowidget(".question_frame.question_text")
+    insert_text(question_widget, question[1])
+    sleep(time_question)
+    indices = [i for i in range(2, 6)]
+    answers = ["A", "B", "C", "D"]
+    for i in range(4):
+        write_answer(question, indices, 2, 5-i, "answer_" + answers[i])
+        sleep(time_answers)
+
+
+def next_question():
+    clear_question_and_answer_boxes()
+    music = game.get_music_for_question()
+    if music[0] is not None:
+        play(music[0])
+        sleep(5)
+    play(music[1])
+    question = game.choose_random_question()
+    write_question(question)
+    game.question_shown = True
+    print("AND HERE")
+
+
 def lets_play():
-    game = Game(guaranteed_questions)
-    print(game)
+    clear_question_and_answer_boxes()
+    game_pointer = game.question_number-1
+    if game_pointer > 0:
+        update_game_tree(game.question_number-1)
+    game.game_on = True
+    if game.game_on:
+        next_question()
 
 
 if __name__ == "__main__":
@@ -152,8 +328,8 @@ if __name__ == "__main__":
 
     pygame.init()
 
-    game_frame = tk.Frame(gui)
-    question_frame = tk.Frame(gui, bg=blue)
+    game_frame = tk.Frame(gui, name="game_frame")
+    question_frame = tk.Frame(gui, bg=blue, name="question_frame")
 
     game_frame.grid(row=0, column=0)
     question_frame.grid(row=0, column=1)
@@ -161,7 +337,8 @@ if __name__ == "__main__":
     play_intro()
     set_lifelines()
     guaranteed_questions = parse_tree()
+    game = Game(guaranteed_questions)
     set_game_button()
     set_question_box()
-    set_answer_boxes()
+    set_answer_boxes(game)
     gui.mainloop()
